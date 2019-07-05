edaef36fc4bac664735e27bbe7db49d00a966a66
Luis Morales <luismorales@io.localdomain>
2019-07-05 16:11:39 +0200

    fix race condition

--- httpstat.go
+++ httpstat.go
@@ -5,14 +5,19 @@ package httpstat
 import (
 	"bytes"
 	"context"
+	"crypto/tls"
 	"fmt"
 	"io"
+	"sync"
+	"net/http/httptrace"
 	"strings"
 	"time"
 )
 
 // Result stores httpstat info.
 type Result struct {
+	m *sync.Mutex
+
 	// The following are duration for each phase
 	DNSLookup        time.Duration
 	TCPConnection    time.Duration
@@ -130,4 +135,158 @@ func (r Result) Format(s fmt.State, verb rune) {
 // time of each httptrace hooks.
 func WithHTTPStat(ctx context.Context, r *Result) context.Context {
 	return withClientTrace(ctx, r)
+}
+
+// End sets the time when reading response is done.
+// This must be called after reading response body.
+func (r *Result) End(t time.Time) {
+	r.trasferDone = t
+
+	// This means result is empty (it does nothing).
+	// Skip setting value(contentTransfer and total will be zero).
+	if r.dnsStart.IsZero() {
+		return
+	}
+
+	r.contentTransfer = r.trasferDone.Sub(r.transferStart)
+	r.total = r.trasferDone.Sub(r.dnsStart)
+}
+
+// ContentTransfer returns the duration of content transfer time.
+// It is from first response byte to the given time. The time must
+// be time after read body (go-httpstat can not detect that time).
+func (r *Result) ContentTransfer(t time.Time) time.Duration {
+	return t.Sub(r.serverDone)
+}
+
+// Total returns the duration of total http request.
+// It is from dns lookup start time to the given time. The
+// time must be time after read body (go-httpstat can not detect that time).
+func (r *Result) Total(t time.Time) time.Duration {
+	return t.Sub(r.dnsStart)
+}
+
+func withClientTrace(ctx context.Context, r *Result) context.Context {
+	r.m = &sync.Mutex{}
+
+	return httptrace.WithClientTrace(ctx, &httptrace.ClientTrace{
+		DNSStart: func(i httptrace.DNSStartInfo) {
+			r.m.Lock()
+			defer r.m.Unlock()
+
+			r.dnsStart = time.Now()
+			r.t0 = r.dnsStart
+		},
+
+		DNSDone: func(i httptrace.DNSDoneInfo) {
+			r.m.Lock()
+			defer r.m.Unlock()
+
+			r.dnsDone = time.Now()
+
+			r.DNSLookup = r.dnsDone.Sub(r.dnsStart)
+			r.NameLookup = r.dnsDone.Sub(r.dnsStart)
+		},
+
+		ConnectStart: func(_, _ string) {
+			r.m.Lock()
+			defer r.m.Unlock()
+
+			r.tcpStart = time.Now()
+
+			// When connecting to IP (When no DNS lookup)
+			if r.dnsStart.IsZero() {
+				r.dnsStart = r.tcpStart
+				r.dnsDone = r.tcpStart
+			}
+		},
+
+		ConnectDone: func(network, addr string, err error) {
+			r.m.Lock()
+			defer r.m.Unlock()
+
+			r.tcpDone = time.Now()
+
+			r.TCPConnection = r.tcpDone.Sub(r.tcpStart)
+			r.Connect = r.tcpDone.Sub(r.dnsStart)
+		},
+
+		TLSHandshakeStart: func() {
+			r.m.Lock()
+			defer r.m.Unlock()
+
+			r.isTLS = true
+			r.tlsStart = time.Now()
+		},
+
+		TLSHandshakeDone: func(_ tls.ConnectionState, _ error) {
+			r.m.Lock()
+			defer r.m.Unlock()
+
+			r.tlsDone = time.Now()
+
+			r.TLSHandshake = r.tlsDone.Sub(r.tlsStart)
+			r.Pretransfer = r.tlsDone.Sub(r.dnsStart)
+		},
+
+		GotConn: func(i httptrace.GotConnInfo) {
+			r.m.Lock()
+			defer r.m.Unlock()
+
+			// Handle when keep alive is used and connection is reused.
+			// DNSStart(Done) and ConnectStart(Done) is skipped
+			if i.Reused {
+				r.isReused = true
+			}
+		},
+
+		WroteRequest: func(info httptrace.WroteRequestInfo) {
+			r.m.Lock()
+			defer r.m.Unlock()
+
+			r.serverStart = time.Now()
+
+			// When client doesn't use DialContext or using old (before go1.7) `net`
+			// pakcage, DNS/TCP/TLS hook is not called.
+			if r.dnsStart.IsZero() && r.tcpStart.IsZero() {
+				now := r.serverStart
+
+				r.dnsStart = now
+				r.dnsDone = now
+				r.tcpStart = now
+				r.tcpDone = now
+			}
+
+			// When connection is re-used, DNS/TCP/TLS hook is not called.
+			if r.isReused {
+				now := r.serverStart
+
+				r.dnsStart = now
+				r.dnsDone = now
+				r.tcpStart = now
+				r.tcpDone = now
+				r.tlsStart = now
+				r.tlsDone = now
+			}
+
+			if r.isTLS {
+				return
+			}
+
+			r.TLSHandshake = r.tcpDone.Sub(r.tcpDone)
+			r.Pretransfer = r.Connect
+		},
+
+		GotFirstResponseByte: func() {
+			r.m.Lock()
+			defer r.m.Unlock()
+
+			r.serverDone = time.Now()
+
+			r.ServerProcessing = r.serverDone.Sub(r.serverStart)
+			r.StartTransfer = r.serverDone.Sub(r.dnsStart)
+
+			r.transferStart = r.serverDone
+		},
+	})
 }
